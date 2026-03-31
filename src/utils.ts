274c97130ae6b0039de51cdf06d15cbc830057bb
aibayanyu20 <aibayanyu@qq.com>
2026-03-31 11:19:57 +0800

    fix: fix Icon not being centered in certain scenarios

--- src/utils.ts
+++ src/utils.ts
@@ -127,9 +127,7 @@ export const iconStyles = `
   cursor: pointer;
 }
 
-.anticon-spin::before,
 .anticon-spin {
-  display: inline-block;
   -webkit-animation: loadingCircle 1s infinite linear;
   animation: loadingCircle 1s infinite linear;
 }
